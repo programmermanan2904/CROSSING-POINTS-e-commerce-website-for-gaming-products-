fef662a272b5307c5002ae989ce849f2208ca802
Manan Jethwa <manan.jethwa.29004@gmail.com>
2026-02-17 19:00:12 +0530

    Force case sync

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
 import Shop from "./pages/Shop";
-import Dashboard from "./pages/vendor/DashBoard";
+import Dashboard from "./pages/vendor/Dashboard";
 import Products from "./pages/vendor/Products";
-import VendorLayout from "./layouts/VendorLayout";
+import VendorOrders from "./pages/vendor/VendorOrders";
 import Checkout from "./pages/Checkout";
-import VendorOrders from "./pages/vendor/vendorOrders";
+import VendorOrders from "./pages/vendor/VendorOrders";
 
 function App() {
   const location = useLocation();
